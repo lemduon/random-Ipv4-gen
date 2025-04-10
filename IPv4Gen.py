f4d29e2d7eee29ef3101819bced5ca024c28f572
lemduon <lemduon@gmail.com>
2025-04-10 17:12:50 +0700

    Update IPv4Gen.py

--- IPv4Gen.py
+++ IPv4Gen.py
@@ -1,11 +1,11 @@
 from random import *
 from time import sleep
 
-strings = [str(randint(100, 200)),
-           str(randint(1,100)),
-           str(randint(1,100)),
-           str(randint(1,100))]
-
 while True:
-    print('.'.join(strings))
-    sleep(60)
+    string1 = str(randint(1, 200))
+    string2 = str(randint(1,100))
+    string3 = str(randint(1,100))
+    string4 = str(randint(1,100))
+    
+    print('.'.join([string1, string2, string3, string4]))
+    sleep(30)
